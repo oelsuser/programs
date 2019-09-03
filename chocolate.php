14334f790fa15749b86927a056d8b740b262c7fd
Test User <abc@gmail.com>
2019-09-03 16:56:16 +0530

    updated

--- chocolate.php
+++ chocolate.php
@@ -1,4 +1,7 @@
-<?php
-$chocolate = 
-
-?>
+<script type="text/javascript">
+	var amount = prompt("Enter amount to buy chocolate");
+	var chocolate = amount*4;
+	var wrapper = chocolate;
+	var totalChocolate = (wrapper/3)+chocolate;
+	alert("Your chocolates: "+Math.floor(totalChocolate));
+</script>
